3be2fc801399cace2e1f5d86b9c6cd601484ddad
thaissa <thaiissa.muniiz@gmail.com>
2023-03-28 11:23:07 -0300

    resolucao exercicio numero dois

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 const primeiraResposta = document.querySelector('.resposta__um');
+const segundaResposta = document.querySelector('.resposta__dois');
+const terceiraResposta = document.querySelector('.resposta__tres');
+const quartaResposta = document.querySelector('.resposta__quatro');
+const quintaResposta = document.querySelector('.resposta__cinco');
 
 const calculaPrimeiroExercicio = () => {
 
@@ -15,4 +19,33 @@ const calculaPrimeiroExercicio = () => {
     console.log(soma);
 }
 
+const calculaFibonacci = () => {
+
+    const numeroDesejado = parseInt(prompt("informe um número, por favor"));
+    const sequencia = [];
+    let cont = 1;
+
+    while (cont < 20) {
+        sequencia[0] = 0;
+        sequencia[1] = 1;
+
+        let soma = sequencia[cont] + sequencia[cont - 1];
+
+        sequencia.push(soma);
+
+        cont++;
+    }
+
+    if (sequencia.includes(numeroDesejado)) {
+        segundaResposta.innerText = `o número ${numeroDesejado} pertence a sequencia.`
+        console.log('pertence')
+        
+    } else {
+        segundaResposta.innerText = `o número ${numeroDesejado} não pertence a sequencia.`
+        console.log('não pertence')
+    }
+}
+
 calculaPrimeiroExercicio();
+
+calculaFibonacci();
